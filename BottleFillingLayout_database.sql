91cc5c7db4f32133e1f2c58d2e87be379963355a
MichaelMoelle <56434143+MichaelMoelle@users.noreply.github.com>
2020-02-13 17:10:08 +0100

    DATENBANK_UPDATE

--- BottleFillingLayout_database.sql
+++ BottleFillingLayout_database.sql
@@ -30,7 +30,61 @@ CREATE TABLE IF NOT EXISTS `Limonaden_Produziert`
  CREATE TABLE IF NOT EXISTS `Limonaden_Defekt`
  (`FlaschenID` INT NOT NULL,
   `Abfuell-Datum` VARCHAR(30) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS `Bestellungen`
+(`Name` VARCHAR(50) NOT NULL,
+`Palettenanzahl` INT NOT NULL);
+
+CREATE TABLE IF NOT EXISTS `Vorratslager`(
+`Leere Flaschen` INT NOT NULL,
+`Flaschendeckel` INT NOT NULL);
+
+#Befüllen des Lagers mit Komponenten
+INSERT INTO `Vorratslager`(`Leere Flaschen`,`Flaschendeckel`)VALUES
+(50000,50000);
   
+#Erstellung der Trigger zur Wertänderung der Tabelle Vorratslager
+DELIMITER |
+CREATE TRIGGER lager_flaschen_abzug
+BEFORE INSERT ON `Limonaden_Produziert`
+FOR EACH ROW
+BEGIN
+UPDATE `Vorratslager` SET `Leere Flaschen` = `Leere Flaschen` - 1
+AND `Flaschendeckel` = `Flaschendeckel` - 1;
+END |
+DELIMITER ;
+
+DELIMITER |
+CREATE TRIGGER lager_flaschen_abzug2
+BEFORE INSERT ON `Limonaden_Defekt`
+FOR EACH ROW
+BEGIN
+UPDATE `Vorratslager` SET `Leere Flaschen` = `Leere Flaschen` - 1
+AND `Flaschendeckel` = `Flaschendeckel` - 1;
+END |
+DELIMITER ;
+
+#Anlegen einer MySQL-Variable
+
+
+#Erstellen eines Triggers zur Wertveränderung der Tabelle Limonaden_Produziert
+DELIMITER |
+CREATE TRIGGER bestellungen_abzug
+BEFORE INSERT ON `Bestellungen`
+FOR EACH ROW
+BEGIN
+SET @FLASCHENANZAHL := 1;
+SET @FLASCHENANZAHL = NEW.`Palettenanzahl` * 800;
+#Löschen der Flaschen und Paletten
+WHILE @FLASCHENANZAHL > 0
+DO
+DELETE FROM `Limonaden_produziert` WHERE `FlaschenID` = MIN(`FlaschenID`);
+SET @FLASCHENANZAHL = @FLASCHENANZAHL - 1;
+END WHILE;
+END |
+
+SHOW TRIGGERS;
+
 #Anlegen der Unique-Schlüssel zur Sicherung der Datenintegrität
 
 ALTER TABLE `bottlefillinglayout`.`limonaden_produziert`
